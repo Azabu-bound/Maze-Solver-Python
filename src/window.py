2c085b6b85fc3a8ac4414ac7f07635ef858c010c
Azabu-bound <tylerjbailey95@gmail.com>
2024-10-07 21:09:46 -0400

    Initial commit. Added window class and output it to the screen.

---
+++ src/window.py
@@ -0,0 +1,27 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+    
+    self._root = Tk()
+    self._root.title("Maze Solver")
+    
+    self._canvas = Canvas()
+    self._canvas.pack()
+    
+    self._running = False
+
+  def redraw(self):
+    self._root.update_idletasks()
+    self._root.update()
+
+  def wait_for_close(self):
+    self._running = True
+    while self._running:
+      self.redraw()
+
+  def close(self):
+    self._running = False
+    self._root.protocol("WM_DELETE_WINDOW", self.close)
